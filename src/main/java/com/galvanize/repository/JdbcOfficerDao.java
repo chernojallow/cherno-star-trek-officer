6d1d0865da569d20c8a0add3159e75d70f4ee719
Zika Chea <jallsadou@gmail.com>
2020-03-18 17:06:27 -0500

    testing daos

--- src/main/java/com/galvanize/repository/JdbcOfficerDao.java
+++ src/main/java/com/galvanize/repository/JdbcOfficerDao.java
@@ -1,12 +1,101 @@
 package com.galvanize.repository;
 
+import com.galvanize.entities.Officer;
+import com.galvanize.entities.Rank;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public class JdbcOfficerDao {
 
-    // @Autowired
+
+
+
+     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    private SimpleJdbcInsert insertOfficer;
+
+
+
+
+    public JdbcOfficerDao(JdbcTemplate jdbcTemplate) {
+
+        this.jdbcTemplate = jdbcTemplate;
+        insertOfficer = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("OFFICERS")
+                .usingGeneratedKeyColumns("id");
+
+    }
+
+
+    public int countOfficer() {
+        return jdbcTemplate.queryForObject("select count(*)from OFFICERS ", Integer.class);
+    }
+
+
+    public List<Officer> findAllOfficers() {
+        return jdbcTemplate.query("select *  from OFFICERS",
+                (rs,rowNum) -> new Officer(rs.getLong("id"),
+                        Rank.valueOf(rs.getString("officer_rank")),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"))
+
+
+        );
+
+
+    }
+
+    public boolean officerExistBy(Long id) {
+
+        boolean found = false;
+
+        try{
+            found = jdbcTemplate.queryForObject("Select 1 from Officers where id = 1", Boolean.class);
+        } catch (EmptyResultDataAccessException e){
+            //ignore
+        }
+
+        return found;
+
+    }
+
+    public Optional<Officer> findOfficerById(long id) {
+        if(!officerExistBy(id)) return Optional.empty();
+        return Optional.ofNullable(jdbcTemplate.queryForObject("select * from OFFICERS where id =?",
+                ((rs,rowNum) -> new Officer(rs.getLong("id"),
+                        Rank.valueOf(rs.getString("officer_rank")),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"))),id)
+        );
+    }
+
+
+    public Officer save(Officer officer) {
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("officer_rank", officer.getRank());
+        parameters.put("first_name", officer.getFirst());
+        parameters.put("last_name", officer.getLast());
+
+        long newId = insertOfficer.executeAndReturnKey(parameters).longValue();
+        officer.setId(newId);
+        return officer;
+
+    }
+
+    public void delete(Long id) {
+        jdbcTemplate.update("delete from OFFICERS where id = ? ", id);
+
+
+    }
 }
--- src/test/java/com/galvanize/JdbcOfficerDaoTest.java
+++ src/test/java/com/galvanize/JdbcOfficerDaoTest.java
@@ -1,13 +1,91 @@
 package com.galvanize;
 
 
+import com.galvanize.entities.Officer;
+import com.galvanize.entities.Rank;
 import com.galvanize.repository.JdbcOfficerDao;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class JdbcOfficerDaoTest {
 
     @Autowired
     JdbcOfficerDao jdbcOfficerDao;
+
+    @Test
+    void countOfficersTest(){
+      int count = jdbcOfficerDao.countOfficer();
+      assertEquals(5, count);
+    }
+
+
+    @Test
+    void findAllOfficersTest(){
+
+        List<Officer> officers = jdbcOfficerDao.findAllOfficers();
+        assertFalse(officers.isEmpty());
+        officers.forEach(System.out::println);
+
+    }
+
+    @Test
+    void officerExistByIdTest(){
+
+        boolean exists = jdbcOfficerDao.officerExistBy(3l);
+        assertTrue(exists);
+    }
+
+
+
+    @Test
+    void existsById_doesnt_existsTest(){
+
+        boolean exists = jdbcOfficerDao.officerExistBy(20l );
+        assertTrue(exists);
+
+    }
+
+@Test
+    void findOfficerByIdTest() {
+        Optional<Officer> officer = jdbcOfficerDao.findOfficerById(3l);
+        assertTrue(officer.isPresent());
+        assertNotNull(officer.get().getId());
+        System.out.println(officer);
+
+    }
+
+
+    @Test
+    void createOfficer() {
+        Officer officer = new Officer(Rank.LIEUTENANT, "cherno", "jallow");
+        officer = jdbcOfficerDao.save(officer);
+        assertNotNull(officer);
+        assertNotNull(officer.getId());
+
+        System.out.println(officer);
+
+    }
+
+
+     @Test
+    void deleteOfficer(){
+
+        Officer officer = new Officer(Rank.LIEUTENANT, "cherno", "jallow");
+        officer = jdbcOfficerDao.save(officer);
+        Long id = officer.getId();
+        assertNotNull(id);
+
+        jdbcOfficerDao.delete(id);
+        assertFalse(jdbcOfficerDao.officerExistBy(id));
+    }
+
+
 }
