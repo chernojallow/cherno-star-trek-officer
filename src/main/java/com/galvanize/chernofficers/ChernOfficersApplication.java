a4121702a820433894046382222ddb077deba199
Zika Chea <jallsadou@gmail.com>
2020-03-17 22:28:13 -0500

    initial commit

---
+++ src/main/java/com/galvanize/chernofficers/ChernOfficersApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.chernofficers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChernOfficersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChernOfficersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galvanize/chernofficers/ChernOfficersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.chernofficers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChernOfficersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
